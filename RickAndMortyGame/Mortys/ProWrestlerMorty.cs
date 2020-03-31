259b7ea5da078acb425ca70df59eee64b8c82655
SMackeyCoding <59894617+SMackeyCoding@users.noreply.github.com>
2020-03-31 19:02:36 -0400

    Add files via upload
    Added Pro-Wrestler Morty.

---
+++ RickAndMortyGame/Mortys/ProWrestlerMorty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static RickAndMortyGame.Attack;
+
+namespace RickAndMortyGame.Mortys
+{
+    class ProWrestlerMorty : IMorty
+    {
+        public int Health { get; set; } = 25;
+
+        public Attack Attack()
+        {
+            return new Attack(10, 20, DamageType.Bludgeoning);
+        }
+
+        public void Hurt(Attack damage)
+        {
+            if (damage.Type == DamageType.Piercing || damage.Type == DamageType.Fire)
+            {
+                damage.Damage = (int)Math.Floor((double)damage.Damage * 2);
+            }
+            else if (damage.Type == DamageType.Bludgeoning || damage.Type == DamageType.Psych)
+            {
+                damage.Damage = (int)Math.Ceiling(damage.Damage * 0.5);
+            }
+            Health -= damage.Damage;
+        }
+
+        public void Scream()
+        {
+            Console.WriteLine("I'M GONNA BRING THE PAIN, OHHHH YEAHHHHH!!!");
+        }
+    }
+}
